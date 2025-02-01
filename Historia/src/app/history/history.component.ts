3403b30fef587379e76e95386c2b50506b438125
@Andrew1200 <anragar20@gmail.com>
2025-02-01 22:49:18 +0100

    Historia de la madre (1 / 5)
    Primer a historia de la madre hecha, falta retocarla para que quede mejor.
    Hay que cambiar el csv para generar una mayor atmósfera

--- Historia/src/app/history/history.component.ts
+++ Historia/src/app/history/history.component.ts
@@ -2,15 +2,44 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-history',
-  imports: [],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.css'
+  styleUrls: ['./history.component.css']
 })
 export class HistoryComponent {
   currentScene: string = 'scene1'; // Escena inicial
+  dudar: boolean = false;
+  preguntar: boolean = false;
+  availableNames: string[] = ['Tomás', 'Juan', 'Pedro', 'Rodrigo', 'Andrés'];
+  contador: number = 0;
+  escenasSiguientes: string[] = ['scene7-1-1', 'scene7-1-2', 'scene7-1-3'];
 
   // Método para cambiar de escena
   nextScene(scene: string) {
     this.currentScene = scene;
   }
+
+  preguntarYAvanzar() {
+    this.preguntar = true;
+    this.nextScene('scene5-2');
+  }
+
+  selectName(selectedName: string) {
+    // Eliminar el nombre seleccionado de la lista
+    this.availableNames = this.availableNames.filter(name => name !== selectedName);
+
+    // Avanzar a la siguiente escena según el contador
+    if (this.contador < this.escenasSiguientes.length) {
+      this.nextScene(this.escenasSiguientes[this.contador]);
+    }
+
+    this.contador++;
+  }
+
+  dudarYAvanzar() {
+    this.dudar = true;
+    this.nextScene('scene5-3');
+  }
+  trackByFn(index: number, item: string): string {
+    return item;
+  }  
 }
